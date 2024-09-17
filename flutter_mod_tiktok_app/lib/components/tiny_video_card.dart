95688c617b9622efc766cc814fe4eff3f28cd658
yuchi <kirklan0423@gmail.com>
2024-09-17 15:12:23 +0800

    短片卡片組件

---
+++ flutter_mod_tiktok_app/lib/components/tiny_video_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mod_tiktok_app/components/avatar_role_name.dart';
+import 'package:flutter_mod_tiktok_app/components/comment_like_read.dart';
+import 'package:flutter_mod_tiktok_app/models/video_model.dart';
+
+class TinyVideoCard extends StatelessWidget {
+  final VideoItem videoItem;
+  const TinyVideoCard({super.key, required this.videoItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _cover(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 8,
+          ),
+          child: AvataRoleName(
+            coverPictureUrl: videoItem.user!.coverPictureUrl,
+            nickname: videoItem.user!.nickname,
+            type: videoItem.user!.type,
+          ),
+        ),
+        CommentLikeRead(
+          commentCount: videoItem.commentCount,
+          thumbUpCount: videoItem.thumbUpCount,
+          readCount: videoItem.readCount,
+        ),
+      ],
+    );
+  }
+
+  Widget _cover() {
+    return AspectRatio(
+      aspectRatio: 9 / 16,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusDirectional.circular(10),
+            child: FadeInImage.assetNetwork(
+              placeholder: 'assets/images/common/lazy-3.png',
+              image: videoItem.coverPictureUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              'assets/images/icons/play_plus.png',
+              width: 38,
+              height: 38,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
